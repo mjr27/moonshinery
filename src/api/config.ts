9e3cec1a4708d582a82c1ca491565a53bf832d88
Kyrylo Kobets <mjr@itage.biz>
2023-02-02 01:47:29 +0200

    Github pages automated deployment

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,5 +1,5 @@
 import urlJoin from 'url-join';
 
-export const ROOT_URI = "http://" + process.env.REACT_APP_ESP32_HOSTNAME;
+export const ROOT_URI: string = process.env.REACT_APP_ESP32_HOST ?? "https://esp32.local";
 export const getUri = (path: string): string => urlJoin(ROOT_URI, path);
 
